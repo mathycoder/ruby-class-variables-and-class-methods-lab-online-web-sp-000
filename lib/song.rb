db54bb2cf9884a0d9322a4d6ca7181fd3a4aa415
Adam S <adam.sarli@gmail.com>
2019-03-15 21:57:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,12 @@ class Song
   attr_accessor :name, :artist, :genre 
   @@count = 0 
   @@genres = [] 
+  @@artists = [] 
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
+    @@artists << artist if !(@@artists.include?(artist))
     @genre = genre 
     @@genres << genre if !(@@genres.include?(genre))
     @@count += 1 
